424196ac3216f463f9b510599c425fc90a27bddb
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2023-11-26 18:13:22 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorApp31/BlazorApp31.Client/Program.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddFluentUIComponents();
+await builder.Build().RunAsync();
